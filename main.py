68cc52cfab4345aae599ab00bc537cdba08b289f
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-03-21 12:23:34 -0500

    displaying all blogs on home route

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    post_url = 'https://api.npoint.io/c790b4d5cab58020d391'
+    response = requests.get(url=post_url)
+    response.raise_for_status()
+    post_data = response.json()
+    return render_template("index.html", posts = post_data)
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=9000)
